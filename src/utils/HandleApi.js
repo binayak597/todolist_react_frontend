78610163e7bd590eed1d470e1aea541f35513559
Binayak Mukherjee <binayak.mukherjee9640@gmail.com>
2023-03-14 18:34:47 +0530

    some minor changes have done

--- src/utils/HandleApi.js
+++ src/utils/HandleApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseUrl = "https://todolist-backend-mu85.onrender.com";
+const baseUrl = "https://todolist-backend-n721.onrender.com";
 
 function getAllToDo(setToDo){
     axios
